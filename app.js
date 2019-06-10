97b7fca65db3a8e572b90c8b83f67179eebcd0a1
Tyler Rivenburgh <rivenburgh.t@husky.neu.edu>
2019-06-10 12:42:19 -0400

    updating port and package.json

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/api/auth', function(req, res) {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
