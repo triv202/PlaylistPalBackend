21e7bcf0842245e80abc0400fcc12403fd94e0ca
Tyler Rivenburgh <rivenburgh.t@husky.neu.edu>
2019-07-15 18:17:48 -0400

    https -> http

--- app.js
+++ app.js
@@ -123,13 +123,13 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('https://playlist-pal.com/recommendations/#' +
+        res.redirect('http://playlist-pal.com/recommendations/#' +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token
             }));
       } else {
-        res.redirect('https://playlist-pal.com/recommendations/#' +
+        res.redirect('http://playlist-pal.com/recommendations/#' +
             querystring.stringify({
               error: 'invalid_token'
             }));
